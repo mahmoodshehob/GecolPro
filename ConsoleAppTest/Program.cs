d2dbac249e72174f3faa6fc5f272a6b9fcadcd5a
Mohammed Elgadri <eng.elgadri@gmail.com>
2024-05-12 13:10:19 +0200

    Create TestApp

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ClassLibrary.GecolSystem_Update;
+using ClassLibrary.GecolSystem_Update.Models;
+
+IGecolServices services = new GecolServices();
+
+var login = services.LoginReqOp();
+
+Console.ReadLine();
