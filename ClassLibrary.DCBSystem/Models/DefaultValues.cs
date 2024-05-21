549bf497c6c7c1fdd6b30450ddefcd421bd177c0
Mahmoud E.Shehob <mahmoodshehob1225@gmail.com>
2024-05-21 02:58:32 +0200

    a

---
+++ ClassLibrary.DCBSystem/Models/DefaultValues.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary.DCBSystem.Models
+{
+    internal class DefaultValues
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Url { get; set; }
+    }
+}
