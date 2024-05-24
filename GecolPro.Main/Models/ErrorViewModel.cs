2961f7976ff61eb2f2a220ae605ddf0b534a330d
Mahmoud E.Shehob <mahmoodshehob1225@gmail.com>
2024-05-24 05:38:45 +0200

    add new class UssdProcessV3

--- GecolPro.Main/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GecolPro.Main.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
