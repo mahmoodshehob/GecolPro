0152aebbbb0903eff6a47ba6011de6cee0f6eec5
Mahmoud E.Shehob <mahmoodshehob1225@gmail.com>
2024-06-10 00:43:07 +0200

    Add Swagger

--- WebApI.TestDataAccess/Program.cs
+++ WebApI.TestDataAccess/Program.cs
@@ -2,6 +2,8 @@ using ClassLibrary.DataAccess;
 using ClassLibrary.DataAccess.Interfaces;
 using ClassLibrary.DataAccess.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,11 +18,33 @@ builder.Services.AddScoped<IMeterService, MeterService>();
 builder.Services.AddScoped<IRequestService, RequestService>();
 
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Gecol API",
+        
+    });
 
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
 
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Set Swagger UI at app's root
+});
 
 
 
